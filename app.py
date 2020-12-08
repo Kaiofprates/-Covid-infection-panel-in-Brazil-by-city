ee27f3d9c54ca7c4baa929e3f53411c3dc525219
Kaiofprates <kaioprates@outlook.com>
2020-12-07 22:20:24 -0300

    improve charts

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
+import base64
 
 st.title('Painel do Covid por cidade')
 st.header('Dados captados da base de dados brasileira disponível em: https://covid.saude.gov.br/')
 
 test = pd.read_csv('./HIST_PAINEL_COVIDBR_02dez2020.csv', sep=";",error_bad_lines=False)
 
+#municipios = df[['municipio','codmun']].groupby('municipio', as_index=True
 
 with open('./dict.p', 'rb') as fp:
     mun = pickle.load(fp)
@@ -19,6 +21,17 @@ choice = st.selectbox(
 
 st.write(' Sua cidade escolhida foi: ', mun['codmun'][choice])
 
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="download.csv">Download csv file</a>'
+    return href
+
 if st.checkbox('Gerar data frame'):
 
     index  = [
@@ -26,8 +39,7 @@ if st.checkbox('Gerar data frame'):
          'codmun','data','semanaEpi',
          'populacaoTCU2019','casosAcumulado',
          'casosNovos','obitosAcumulado',
-         'obitosNovos','Recuperadosnovos',
-         'emAcompanhamentoNovos']
+         'obitosNovos']
 
     cit = test[index].where(test['codmun']  == mun['codmun'][choice])
     cit.dropna(subset=['codmun'], inplace=True)
@@ -37,11 +49,19 @@ if st.checkbox('Gerar data frame'):
         obitos = cit[['casosNovos','obitosNovos']]
         st.line_chart(obitos)
 
-    semana  = cit.groupby('semanaEpi', as_index=False)[['casosNovos','obitosNovos']].mean()
-    st.write('Média de casos novos  e obitos novos por semana epidemiológica')
+    semana  = cit.groupby('semanaEpi', as_index=True)[['casosNovos','obitosNovos']].mean()
+    st.write('Média de casos novos  e óbitos novos por semana epidemiológica')
     st.line_chart(semana[['casosNovos','obitosNovos']])
-    semana  = cit.groupby('semanaEpi', as_index=False)[['casosNovos','obitosNovos']].sum()
+    semana  = cit.groupby('semanaEpi', as_index=True)[['casosNovos','obitosNovos']].sum()
+    st.write('Maior número de infectados por semana ', semana['casosNovos'].max())
+    st.write('Maior número de óbitos por semana ', semana['obitosNovos'].max())
+    st.write('Rank das semanas com maior numero de infecção')
+    st.write(semana.nlargest(10,'casosNovos'))
+    st.write('Rank das semanas com menor número de infecção')
+    st.write(semana.nsmallest(10,'casosNovos'))
     st.write('Total de mortes ' + str(semana['obitosNovos'].sum()))
     st.line_chart(semana['obitosNovos'])
     st.write('Total de casos ' + str(semana['casosNovos'].sum()))
     st.line_chart(semana['casosNovos'])
+    if (st.checkbox('Salvar Data Frame')):
+        st.markdown(get_table_download_link(cit), unsafe_allow_html=True)
